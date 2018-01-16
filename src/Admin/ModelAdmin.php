ce865204bc9a0dc4ae061c97a877f748db35d7c1
zauberfisch <admin@zauberfisch.at>
2018-01-16 01:05:01 +0000

    ModelAdmin can now handle both DataObjects and PersistentDataObjects

--- src/Admin/ModelAdmin.php
+++ src/Admin/ModelAdmin.php
@@ -12,27 +12,30 @@ class ModelAdmin extends \ModelAdmin {
 	public function getEditForm($id = null, $fields = null) {
 		/** @var \Form $return */
 		$return = parent::getEditForm($id, $fields);
-		/** @var \GridField $grid */
-		$grid = $return->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
-		$config = new Config_RecordEditor();
-		$config->removeComponentsByType(\GridFieldFilterHeader::class);
-		// Validation
-		if (singleton($this->modelClass)->hasMethod('getCMSValidator')) {
-			$detailValidator = singleton($this->modelClass)->getCMSValidator();
-			/** @var DetailForm $detailForm */
-			$detailForm = $config->getComponentByType(DetailForm::class);
-			$detailForm->setValidator($detailValidator);
+		if (is_a($this->modelClass, \PersistentDataObject_Model_DataObject::class)) {
+
+			/** @var \GridField $grid */
+			$grid = $return->Fields()->fieldByName($this->sanitiseClassName($this->modelClass));
+			$config = new Config_RecordEditor();
+			$config->removeComponentsByType(\GridFieldFilterHeader::class);
+			// Validation
+			if (singleton($this->modelClass)->hasMethod('getCMSValidator')) {
+				$detailValidator = singleton($this->modelClass)->getCMSValidator();
+				/** @var DetailForm $detailForm */
+				$detailForm = $config->getComponentByType(DetailForm::class);
+				$detailForm->setValidator($detailValidator);
+			}
+			// Import / Export
+			$config->addComponent((new \GridFieldExportButton('buttons-before-left'))->setExportColumns($this->getExportFields()));
+			//if ($this->showImportForm) {
+			//	$config->addComponent(
+			//		GridFieldImportButton::create('buttons-before-left')
+			//			->setImportForm($this->ImportForm())
+			//			->setModalTitle(_t('ModelAdmin.IMPORT', 'Import from CSV'))
+			//	);
+			//}
+			$grid->setConfig($config);
 		}
-		// Import / Export
-		$config->addComponent((new \GridFieldExportButton('buttons-before-left'))->setExportColumns($this->getExportFields()));
-		//if ($this->showImportForm) {
-		//	$config->addComponent(
-		//		GridFieldImportButton::create('buttons-before-left')
-		//			->setImportForm($this->ImportForm())
-		//			->setModalTitle(_t('ModelAdmin.IMPORT', 'Import from CSV'))
-		//	);
-		//}
-		$grid->setConfig($config);
 		return $return;
 	}
 }
